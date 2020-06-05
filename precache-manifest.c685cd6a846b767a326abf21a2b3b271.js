91a821ac522e3f537d901c720cff096469dc901d
Rishik Zaparde <rishikzaparde@gmail.com>
2020-06-05 16:56:45 -0400

    Updates

---
+++ precache-manifest.c685cd6a846b767a326abf21a2b3b271.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb03e9073f139774ff8aacca294c68e2",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e5af8cd9b37992e2588c",
+    "url": "/weather-app/static/css/main.9a0092d5.chunk.css"
+  },
+  {
+    "revision": "df0b7e2bc16c817feda1",
+    "url": "/weather-app/static/js/2.25f7c1bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.25f7c1bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5af8cd9b37992e2588c",
+    "url": "/weather-app/static/js/main.07e1aebc.chunk.js"
+  },
+  {
+    "revision": "ede5d68a1a243dbbac5b",
+    "url": "/weather-app/static/js/runtime-main.3488f0a4.js"
+  },
+  {
+    "revision": "78c28465643a20597ce65eee037a7675",
+    "url": "/weather-app/static/media/HelveticaNeue Thin.78c28465.ttf"
+  }
+]);
